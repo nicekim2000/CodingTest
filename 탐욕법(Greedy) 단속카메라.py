013fbeb598bdcb62f433402ced2fff9fac9d28ad
nicekim2000 <rlawogur0208@naver.com>
2023-12-15 17:02:07 +0900

    12/15

---
+++ 탐욕법(Greedy) 단속카메라.py
@@ -0,0 +1,15 @@
+def solution(routes):
+    routes = sorted(routes, key=lambda x: x[1])
+    last_camera = -30000
+
+    answer = 0
+
+    for route in routes:
+        if last_camera < route[0]:
+            answer += 1
+            last_camera = route[1]
+
+    return answer
+
+a=solution(	[[-20, -15], [-14, -5], [-18, -13], [-5, -3]])
+print(a)
